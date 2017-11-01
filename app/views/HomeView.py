da2f40cf643058ffc83130befbc29f8ca0780e30
Alberto Wagner <alberto.oliveira@ccc.ufcg.edu.br>
2017-11-01 20:31:29 -0200

    PPagina inicial pronta para desenvolver

---
+++ app/views/HomeView.py
@@ -0,0 +1,10 @@
+"""HomeView.py: Especifica a pagina inicial da aplicacao."""
+
+__author__ = "Alberto Wagner"
+__copyright__ = "Copyright 2017, LES-UFCG"
+
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
--- trocadobem/urls.py
+++ trocadobem/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from app.views.HomeView import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^admin/', admin.site.urls),
     url(r'^admin/login/$', auth_views.login),
+    url(r'^$', HomeView.as_view(), name='home')
 ]
