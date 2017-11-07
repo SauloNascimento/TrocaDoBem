6650207610020d44c90e1df358d16c2776935df6
João Marcos <joao.almeida@ccc.ufcg.edu.br>
2017-11-07 14:00:01 -0200

    Modificações para diminuir as violações do flake8.

--- app/views/CadastroInstituicaoView.py
+++ app/views/CadastroInstituicaoView.py
@@ -1,7 +1,8 @@
-from django.views.generic import TemplateView
-from app.models import Instituicao
-from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
+
+from app.models import Instituicao
 
 __author__ = "Raphaeldobu"
 __copyright__ = "Copyright 2017, LES-UFCG"
@@ -10,7 +11,12 @@ __copyright__ = "Copyright 2017, LES-UFCG"
 class CadastroInstituicaoView(TemplateView):
     template_name = 'cadastros/cadastro-instituicao.html'
 
-def cadastroInst(request):
-    inst = Instituicao(photo=request.POST['photo'], cnpj=request.POST['cnpj'], name=request.POST['name'], cep=request.POST['photo'], estado=request.POST['estado'], cidade=request.POST['cidade'], bairro=request.POST['bairro'], rua=request.POST['rua'], numero=request.POST['numero'], complemento=request.POST['complemento'], descricao=request.POST['descricao'], email=request.POST['email'], telefone=request.POST['telefone'])
+
+def cadastro_inst(request):
+    inst = Instituicao(photo=request.POST['photo'], cnpj=request.POST['cnpj'], name=request.POST['name'],
+                       cep=request.POST['photo'], estado=request.POST['estado'], cidade=request.POST['cidade'],
+                       bairro=request.POST['bairro'], rua=request.POST['rua'], numero=request.POST['numero'],
+                       complemento=request.POST['complemento'], descricao=request.POST['descricao'],
+                       email=request.POST['email'], telefone=request.POST['telefone'])
     inst.save()
     return HttpResponseRedirect(reverse('home', args=()))
