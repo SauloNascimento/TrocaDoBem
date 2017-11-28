b63fbcae1b573c9a4fdcb3b128a4e86aed3ea779
Caio Marinho <caiomarinho8@gmail.com>
2017-11-27 21:05:52 -0300

    Tasks #19305, #19308, #19309, #19319, #19320, #19321

--- app/tests/test_register_institute.py
+++ app/tests/test_register_institute.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
 
 
-class TestRegisterInstituicaoDefault(unittest.TestCase):
+class TestRegisterInstitute(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -16,47 +16,35 @@ class TestRegisterInstituicaoDefault(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_register_instituicao_default(self):
+    def test_register_institute(self):
         driver = self.driver
         driver.get(self.base_url + "/")
-        time.sleep(1)
         driver.find_element_by_id("inscrevase_link").click()
         driver.find_element_by_id("register_instituicao_link").click()
-        time.sleep(1)
         driver.find_element_by_id("id_first_name").clear()
-        driver.find_element_by_id("id_first_name").send_keys("Instituicao 1")
+        driver.find_element_by_id("id_first_name").send_keys("Instituicao " + str(uuid.uuid4()))
         driver.find_element_by_id("id_cnpj").clear()
-        driver.find_element_by_id("id_cnpj").send_keys("27.974.463/0001-00")
+        driver.find_element_by_id("id_cnpj").send_keys("26.865.771/0001-35")
         driver.find_element_by_id("id_cep").clear()
-        driver.find_element_by_id("id_cep").send_keys("12345654")
-        driver.find_element_by_id("id_state").clear()
-        driver.find_element_by_id("id_state").send_keys("PB")
-        driver.find_element_by_id("id_city").clear()
-        driver.find_element_by_id("id_city").send_keys("Campina grande")
-        driver.find_element_by_id("id_district").clear()
-        driver.find_element_by_id("id_district").send_keys("Tres Irmas")
-        driver.find_element_by_id("id_address").clear()
-        driver.find_element_by_id("id_address").send_keys(u"Rua Cláudio Bezerra de Lima")
+        driver.find_element_by_id("id_cep").send_keys("58.423-530")
         driver.find_element_by_id("id_number").clear()
-        driver.find_element_by_id("id_number").send_keys("694")
-        driver.find_element_by_id("id_phone").clear()
-        driver.find_element_by_id("id_phone").send_keys("(83) 9887 31795")
+        driver.find_element_by_id("id_number").send_keys("199")
+        time.sleep(5)
         driver.find_element_by_id("id_complement").clear()
-        driver.find_element_by_id("id_complement").send_keys("Nenhum")
+        driver.find_element_by_id("id_complement").send_keys("Perto da Padaria")
+        driver.find_element_by_id("id_phone").clear()
+        driver.find_element_by_id("id_phone").send_keys("(11)11111-1111")
         driver.find_element_by_id("id_email").clear()
-        driver.find_element_by_id("id_email").send_keys("teste@gmail.com")
+        driver.find_element_by_id("id_email").send_keys("instituicao@gmail.com")
         driver.find_element_by_id("id_username").clear()
         driver.find_element_by_id("id_username").send_keys("instituicao-" + str(uuid.uuid4()))
         driver.find_element_by_id("id_password").clear()
-        driver.find_element_by_id("id_password").send_keys("abcde")
-        time.sleep(1)
+        driver.find_element_by_id("id_password").send_keys("instituicao")
         driver.find_element_by_id("btn_salvar").click()
         try:
-            self.assertEqual(u"Nova Instituição cadastrada com sucesso.",
-                             driver.find_element_by_xpath("//div[3]/p").text)
+            self.assertEqual("Sucesso", driver.find_element_by_css_selector("h2").text)
         except AssertionError as e:
             self.verificationErrors.append(str(e))
-        time.sleep(1)
         driver.find_element_by_css_selector("button.confirm").click()
 
     def is_element_present(self, how, what):
