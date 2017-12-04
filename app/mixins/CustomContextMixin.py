0cd51dc987f96fb2ca9b3d5cbff9ad0e2152884a
Alberto Wagner <alberto.oliveira@ccc.ufcg.edu.br>
2017-12-04 20:57:17 -0300

    Arrumando a barra de progresso e sist notificacao

--- app/mixins/CustomContextMixin.py
+++ app/mixins/CustomContextMixin.py
@@ -5,10 +5,10 @@ from app.models import Audit, Notification
 
 class CustomContextMixin(ContextMixin):
     def get_context_data(self, **kwargs):
-        if 'audits' not in kwargs:
+        if 'audits_l' not in kwargs:
             kwargs['audits_l'] = Audit.objects.filter(new_owner=self.request.user, is_complete=False).order_by(
                 '-created_at')
-        if 'notifications' not in kwargs:
+        if 'notifications_l' not in kwargs:
             kwargs['notifications_l'] = Notification.objects.filter(user=self.request.user, is_read=False).order_by(
                 '-created_at')
         return kwargs
