ec3bdaf57ef11eb8385b57e19f19f729de14522b
tainahemmanuele <tainahemmanuele@gmail.com>
2018-01-15 20:55:51 -0300

    Primeira parte da view de cadastrado da doação requisitada pronta. Falta a parte de cadastrar a doação

--- app/views/DonatorRequerimentView.py
+++ app/views/DonatorRequerimentView.py
@@ -5,6 +5,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.views.generic import UpdateView
 from django.views.generic import FormView
+from django.views.generic import FormView
+
+from app.forms import FormRegisterUser
+from app.models import CommonUser
 
 
 from app.forms import FormDonatorUpdate
@@ -45,3 +49,43 @@ class DonatorRequerimentView( LoginRequiredMixin, UpdateView, UserContextMixin):
     def form_invalid(self, form):
         print(form.errors)
         return super(DonatorRequerimentView, self).form_invalid(form)
+
+class DonatorRequerimentViewAnonymous(FormView):
+    """
+    Displays the login form.
+    """
+    template_name = 'donator_requeriment_view.html'
+    form_class = FormRegisterUser
+    success_url = '/login'
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        aux_obj = User.objects.filter(username=data['username'])
+        if len(aux_obj) > 0:
+            return self.form_invalid(form)
+        user_data = {}
+        common_data = {}
+        user_data['first_name'] = data['first_name']
+        user_data['email'] = data['email']
+        user_data['username'] = data['username']
+        common_data['cpf'] = data['cpf']
+        common_data['phone'] = data['phone']
+        common_data['anonymous'] = data['anonymous']
+        if data['username'] and data['password']:
+            new_user = User.objects.create_user(**user_data)
+            new_common_user = CommonUser(user=new_user, **common_data)
+            new_common_user.save()
+        else:
+            return self.form_invalid(form)
+        return super(DonatorRequerimentViewAnonymous, self).form_valid(form)
+
+    def form_invalid(self, form):
+        print(form.errors)
+        return super(DonatorRequerimentViewAnonymous, self).form_invalid(form)
--- trocadobem/urls.py
+++ trocadobem/urls.py
@@ -29,7 +29,7 @@ from app.views.ShowcaseRequerimentView import RequirementShowCaseView , Requerim
 
 from app.views.ChangePasswordView import ChangePasswordView
 
-from app.views.DonatorRequerimentView import  DonatorRequerimentView
+from app.views.DonatorRequerimentView import  DonatorRequerimentView, DonatorRequerimentViewAnonymous
 
 
 __author__ = "Caio Marinho"
@@ -122,7 +122,7 @@ urlpatterns = [
 
     url(r'^showcase/$',RequirementShowCaseView, name='list_showcase'),
     url(r'^showcase/(?P<requeriment_id>\d+)/$', RequerimentDetail, name = 'detail'),
-    url(r'^donationrequeriment/$', DonatorRequerimentView.as_view(), name='donation_requeriment_1'),
+    url(r'^donationrequeriment/None$', DonatorRequerimentViewAnonymous.as_view(), name='donation_requeriment_1'),
     url(r'^donationrequeriment/(?P<pk>[0-9]+)/$', DonatorRequerimentView.as_view(), name='donation_requeriment'),
     url(r'^account/(?P<pk>[0-9]+)/change-password/$', ChangePasswordView.as_view(), name='change_password'),
 
